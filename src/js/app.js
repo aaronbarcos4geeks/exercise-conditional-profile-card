091927732ffb802656a46052a186bc4f6b55e18f
aaronbarcos4geeks <aaronbarcos4geeks@gmail.com>
2024-10-28 19:14:11 +0000

    Exercise done

--- src/js/app.js
+++ src/js/app.js
@@ -23,27 +23,51 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  console.log("These are the current variables: ", variables);
+
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
+  let fullName = `${variables.name || "First Name"} ${variables.lastName ||
+    "Last Name"}`;
+  let role = variables.role || "Select a Role";
+  let city = variables.city || "City";
+  let country = variables.country || "Country";
+
+  // Posiciona la barra de redes sociales
+  let socialMediaPosition =
+    variables.socialMediaPosition === "position-left"
+      ? "position-left"
+      : "position-right";
+
+  // Genera los enlaces de redes sociales solo si se ha proporcionado un valor
+  let twitterLink = variables.twitter
+    ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
+    : "";
+  let githubLink = variables.github
+    ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+    : "";
+  let linkedinLink = variables.linkedin
+    ? `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+    : "";
+  let instagramLink = variables.instagram
+    ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
+    : "";
+
+  // Genera el HTML dinámico de la tarjeta
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${fullName}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="${socialMediaPosition}">
+            ${twitterLink}
+            ${githubLink}
+            ${linkedinLink}
+            ${instagramLink}
           </ul>
-        </div>
-    `;
+        </div>`;
 }
 
 /**
